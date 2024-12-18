0b5a8a947aebc263c5a1d3516c73e6f7c82ca5f3
sibu <sibuna742@gmail.com>
2024-12-17 21:17:17 -0500

    git1

---
+++ new1.js
@@ -0,0 +1,23 @@
+// let x=1;
+// let n=prompt("enter a number:");
+// let sum=0;
+
+// while(x<=n){
+//     sum=sum+Number(x);
+//     console.log(sum);
+
+// x++;
+// }
+
+
+function greet(){
+ let num=0;
+    while(num<=50){
+        
+        console.log(num);
+
+    }
+   
+
+}
+greet();
